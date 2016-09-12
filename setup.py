2dc394aec1f517de13be7cebb6d09564672a6b74
furion <_@furion.me>
2016-09-12 18:51:52 +0200

    minor tweak to setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Library',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
     ],
