80605b42d98bc2b5e9cc3b6519d8abbe0b7c8b93
furion <_@furion.me>
2016-09-13 21:27:37 +0200

    there is no block 0

--- steemtools/blockchain.py
+++ steemtools/blockchain.py
@@ -28,7 +28,7 @@ class Blockchain(object):
                         "op": op,
                     }
 
-    def replay(self, start_block=0, end_block=None, filter_by=None, **kwargs):
+    def replay(self, start_block=1, end_block=None, filter_by=None, **kwargs):
         """
         :param start_block: Block number of the first block to parse.
         :param end_block: Block number of the last block to parse.
