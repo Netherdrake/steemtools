73582499f8447c40a6b6e9404022d7448db9d94f
furion <_@furion.me>
2016-09-17 00:49:27 +0200

    removed an arbitrary limit from avg_witness_price

--- steemtools/market.py
+++ steemtools/market.py
@@ -12,6 +12,5 @@ class Market(Tickers):
         self.steem = steem
 
     def avg_witness_price(self, take=10):
-        take = 20 if take > 20 else take
         price_history = self.steem.rpc.get_feed_history()['price_history']
         return np.mean([parse_payout(x['base']) for x in price_history[-take:]])
